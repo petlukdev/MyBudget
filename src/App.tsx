2ca972545939aadd91734ff97ecae9aca0fbce05
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-02-25 18:13:34 +0100

    Added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import DashboardCard from './components/DashboardCard'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-gray-100 min-h-screen p-5">
-      <h1>Hello World!</h1>
-      <DashboardCard title='Total Balance' value={1000} />
-      <DashboardCard title='Income' value={-50} />
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col bg-gray-100 min-h-screen p-5">
+        <DashboardCard title='Total Balance' value={1000} />
+        <DashboardCard title='Income' value={-50} />
+      </div>
+    </>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-white py-3 border-b border-gray-200 text-center">
+      <h1 className="text-2xl">My<b>Budget</b></h1>
+    </header>
+  );
+}
+
+export default Header;
