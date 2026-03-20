f8a813d991c1721442e67e7a27da06d9d274bf0a
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-03-20 18:09:28 +0100

    Fixed DashboardCard responsivness + Added date to TransactionHistoryItem

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -9,7 +9,7 @@ function DashboardCard({ title, value = 0 }: { title: string; value: number }) {
         <div className="flex flex-col justify-center items-center 
         bg-white rounded-lg shadow-lg p-4">
         <h2 className="text-xl font-semibold">{title}</h2>
-        <p className={`text-2xl font-bold ${value <= 0 ? 'text-red-500' : 'text-green-500'}`}>
+        <p className={`text-2xl font-bold text-center ${value <= 0 ? 'text-red-500' : 'text-green-500'}`}>
             {value.toLocaleString()} {base}
         </p>
         </div>
--- src/components/TransactionHistoryItem.tsx
+++ src/components/TransactionHistoryItem.tsx
@@ -31,15 +31,19 @@ function TransactionHistoryItem({ transaction, onClick }: { transaction: Transac
     const value = convert(transaction.amount, Currency.EUR, base);
     
     return (
-        <li className="flex items-center gap-3 hover:bg-gray-100 p-1 rounded-lg cursor-pointer transition-colors" onClick={onClick}>
-            <img src={categoryIcons[transaction.category]} alt={transaction.category} 
-            className="w-11 h-11 p-1 bg-gray-200 rounded-md" />
-            <div className="text-sm min-w-0">
-                <p className="font-semibold truncate">{transaction.title}</p>
-                <p className="text-xs text-gray-500">{transaction.category}</p>
+        <li className="grid grid-cols-2 sm:grid-cols-3 items-center gap-3 hover:bg-gray-100 p-1 rounded-lg cursor-pointer transition-colors" onClick={onClick}>
+            <div className="flex flex-row items-center gap-2">
+                <img src={categoryIcons[transaction.category]} alt={transaction.category} 
+                className="w-11 h-11 p-1 bg-gray-200 rounded-md" />
+                <div className="text-sm min-w-0">
+                    <p className="font-semibold truncate">{transaction.title}</p>
+                    <p className="text-xs text-gray-500">{transaction.category}</p>
+                </div>
             </div>
-            <p className={`ml-auto font-bold text-nowrap
-                ${value < 0 ? 'text-red-500' : 'text-green-500'}`}>
+            <p className="hidden text-end text-gray-500 sm:block">
+                {new Date(transaction.date).toLocaleDateString()}
+            </p>
+            <p className={`grid-start-3 text-end font-bold text-nowrap ${value < 0 ? 'text-red-500' : 'text-green-500'}`}>
                     {value.toLocaleString()} {base}
             </p>
         </li>
