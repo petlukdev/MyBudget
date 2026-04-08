59ac9fa922daa92f96db2e6df23168d4765ce0bd
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-04-08 09:49:07 +0200

    Moved main content to MainPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,14 @@
-import { useEffect, useMemo, useState } from 'react'
+import Header from './components/Header'
+import MainPage from './components/MainPage'
+
 import { CurrencyProvider } from './contexts/CurrencyContext'
 
-import type { Transaction } from './types/Transaction'
-
-import Modal from './components/Modal'
-import Header from './components/Header'
-import Dashboard from './components/Dashboard'
-import TransactionHistory from './components/TransactionHistory'
-import AddTransactionForm from './components/AddTransactionForm'
-import AddTransactionButton from './components/AddTransactionButton'
-import AddTransactionContainer from './components/AddTransactionContainer'
-
 function App() {
-  const [isFormOpen, setIsFormOpen] = useState(false);
-  const [transactions, setTransactions] = useState<Transaction[]>(() => {
-    const stored = localStorage.getItem('transactions');
-    return stored ? JSON.parse(stored) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem('transactions', JSON.stringify(transactions));
-  }, [transactions]);
-
-  const stats = useMemo(() => {
-    const totalBalance = transactions.reduce((sum, t) => sum + t.amount, 0);
-    const income = transactions.filter(t => t.amount > 0).reduce((sum, t) => sum + t.amount, 0);
-    const expenses = transactions.filter(t => t.amount < 0).reduce((sum, t) => sum + t.amount, 0);
-    return { totalBalance, income, expenses };
-  }, [transactions]);
-
-  const addTransaction = (transaction: Transaction) => {
-    setTransactions(prev => [transaction, ...prev]);
-    setIsFormOpen(false);
-  }
-
   return (
-    <>
-      <CurrencyProvider>
-        <Header/>
-        <main className="bg-gray-100 min-h-screen p-5">
-          <div className='flex flex-col gap-3 mx-auto lg:container lg:grid lg:grid-rows-[auto_1fr] lg:gap-5'>
-            <section className='lg:max-h-32'>
-              <Dashboard stats={stats}/>
-            </section>
-            <section className='lg:grid lg:grid-cols-3 lg:gap-4 lg:min-h-0'>
-              <AddTransactionContainer addTransaction={addTransaction}/>
-              <TransactionHistory transactions={transactions} setTransactions={setTransactions}/>
-            </section>
-          </div>
-          <AddTransactionButton onClick={() => setIsFormOpen(true)}/>
-        </main>
-        {isFormOpen &&
-          <Modal onClose={() => setIsFormOpen(false)}>
-            <AddTransactionForm addTransaction={addTransaction}/>
-          </Modal>
-        }
-      </CurrencyProvider>
-    </>
+    <CurrencyProvider>
+      <Header/>
+      <MainPage/>
+    </CurrencyProvider>
   )
 }
 
---
+++ src/components/MainPage.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useMemo, useState } from "react";
+import type { Transaction } from "../types/Transaction";
+
+import Modal from "./Modal";
+import Dashboard from "./Dashboard";
+import AddTransactionForm from "./AddTransactionForm";
+import TransactionHistory from "./TransactionHistory";
+import AddTransactionButton from "./AddTransactionButton";
+import AddTransactionContainer from "./AddTransactionContainer";
+
+function MainPage() {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [transactions, setTransactions] = useState<Transaction[]>(() => {
+    const stored = localStorage.getItem('transactions');
+    return stored ? JSON.parse(stored) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }, [transactions]);
+
+  const stats = useMemo(() => {
+    const totalBalance = transactions.reduce((sum, t) => sum + t.amount, 0);
+    const income = transactions.filter(t => t.amount > 0).reduce((sum, t) => sum + t.amount, 0);
+    const expenses = transactions.filter(t => t.amount < 0).reduce((sum, t) => sum + t.amount, 0);
+    return { totalBalance, income, expenses };
+  }, [transactions]);
+
+  const addTransaction = (transaction: Transaction) => {
+    setTransactions(prev => [transaction, ...prev]);
+    setIsFormOpen(false);
+  }
+
+  return (
+    <main className="bg-gray-100 min-h-screen p-5">
+        <div className='flex flex-col gap-3 mx-auto lg:container lg:grid lg:grid-rows-[auto_1fr] lg:gap-5'>
+            <section className='lg:max-h-32'>
+                <Dashboard stats={stats}/>
+            </section>
+            <section className='lg:grid lg:grid-cols-3 lg:gap-4 lg:min-h-0'>
+                <AddTransactionContainer addTransaction={addTransaction}/>
+                <TransactionHistory transactions={transactions} setTransactions={setTransactions}/>
+            </section>
+        </div>
+        <AddTransactionButton onClick={() => setIsFormOpen(true)}/>
+        {isFormOpen &&
+            <Modal onClose={() => setIsFormOpen(false)}>
+                <AddTransactionForm addTransaction={addTransaction}/>
+            </Modal>
+        }
+    </main>
+  );
+}
+
+export default MainPage;
