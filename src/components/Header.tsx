9e083bafbbc171aa7f2874d8c2b4d9625722db8f
Petr Lukáš <157734556+petlukdev@users.noreply.github.com>
2026-04-23 09:59:47 +0200

    Updated Header's accessibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,11 +12,14 @@ function Header() {
     <header className="sticky top-0 z-10 px-5 bg-white border-b border-gray-200 text-center">
       <div className="flex items-center justify-between py-3 mx-auto lg:container">
         <h1 className="text-2xl">My<b>Budget</b></h1>
-        <select defaultValue={base} onChange={handleSetBase}
-        className="bg-white border border-gray-300 rounded p-1">
-          {Object.values(Currency).map((currency) => (
-            <option key={currency} value={currency}>{currency}</option>
-          ))}
+        <select
+          defaultValue={base}
+          onChange={handleSetBase}
+          aria-label="Select currency"
+          className="bg-white border border-gray-300 rounded p-1">
+            {Object.values(Currency).map((currency) => (
+              <option key={currency} value={currency}>{currency}</option>
+            ))}
         </select>
       </div>
     </header>
